89f9d9f89975da29a67eeec49db2d66097b6a506
Uday Bhateja <uday.bhateja@biofourmis.com>
2021-01-07 16:44:57 +0530

    Initial Changes

--- js/form.js
+++ js/form.js
@@ -29,19 +29,18 @@ $(function() {
 			$(formMessages).text('Your message successfully sent');
 
 			// Clear the form.
-			$('#name, #email, #message').val('');			
+			$('#name, #email, #message').val('');
 		})
 		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('bg-success');
-			$(formMessages).addClass('bg-danger');
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('bg-danger');
+			$(formMessages).addClass('bg-success');
 
 			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
+			$(formMessages).text('Your message successfully sent');
+
+			// Clear the form.
+			$('#name, #email, #message').val('');
 		});
 
 	});
